2a7dedf6bd594502df997f541ce4a9c7601aee64
luoshuai <1029113525@qq.com>
2021-06-22 15:38:40 +0800

    feat:建议选项补充

---
+++ src/DatePicker/components/suggestOpts.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import dayjs, { parseTime2Week } from '../utils/dayjs';
+import TodaySvg from '../svg/todaySvg';
+import TomorrowSvg from '../svg/tomorrowSvg';
+import SundaySvg from '../svg/SundaySvg';
+
+interface PropsType {
+    onSave: (x: string) => void;
+}
+export default function suggestOpts(props: PropsType) {
+    useEffect(() => {}, []);
+
+    return (
+        <div className="scheduler-suggestions">
+            <button className="suggestions-item today" onClick={() => props.onSave(dayjs().format('YYYY-MM-DD 00:00'))}>
+                <div className="item-icon">
+                    <TodaySvg day={dayjs().format('DD')} />
+                </div>
+                <div className="item-label ">今天</div>
+                <div className="item-week">{parseTime2Week(dayjs())}</div>
+            </button>
+            <button className="suggestions-item tomorrow" onClick={() => props.onSave(dayjs().add(1, 'd').format('YYYY-MM-DD 00:00'))}>
+                <div className="item-icon">
+                    <TomorrowSvg />
+                </div>
+                <div className="item-label ">明天</div>
+                <div className="item-week">{parseTime2Week(dayjs().add(1, 'd'))}</div>
+            </button>
+            <button className="suggestions-item sunday" onClick={() => props.onSave(dayjs().day(0).add(1, 'w').format('YYYY-MM-DD 00:00'))}>
+                <div className="item-icon">
+                    <SundaySvg />
+                </div>
+                <div className="item-label">周日</div>
+                <div className="item-week">{dayjs().day(0).add(1, 'w').format('MM月DD日')}</div>
+            </button>
+        </div>
+    );
+}
