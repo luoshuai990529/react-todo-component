5515c1074c3c07d7e3ca5efa73b8695fd4bb09f7
luoshuai <1029113525@qq.com>
2021-06-22 19:15:31 +0800

    feat:日期选择样式

--- src/DatePicker/components/datePicker.tsx
+++ src/DatePicker/components/datePicker.tsx
@@ -34,8 +34,77 @@ export default function DatePicker(props: PropsType) {
                     <div>周日</div>
                 </div>
             </div>
-            <div className="scheduler-date-picker-day">
-                <div className="content">123456</div>
+            <div className="scheduler-date-picker-content">
+                <div className="data-picker-month">
+                    <div className="date-picker-month-header">6月</div>
+                    <div className="calendar">
+                        <div className="calendar-week">1 2 3 4</div>
+                        <div className="calendar-week">5 6 7 8 9 10 11</div>
+                        <div className="calendar-week">12 13 14 15 16 17 18</div>
+                        <div className="calendar-week">19 20 21 22 23 24 25</div>
+                        <div className="calendar-week">26 27 28 29 30 31</div>
+                    </div>
+                </div>
+                <div className="data-picker-month">
+                    <div className="date-picker-month-header">7月</div>
+                    <div className="calendar">
+                        <div className="calendar-week">1 2 3 4</div>
+                        <div className="calendar-week">5 6 7 8 9 10 11</div>
+                        <div className="calendar-week">12 13 14 15 16 17 18</div>
+                        <div className="calendar-week">19 20 21 22 23 24 25</div>
+                        <div className="calendar-week">26 27 28 29 30 31</div>
+                    </div>
+                </div>
+                <div className="data-picker-month">
+                    <div className="date-picker-month-header">8月</div>
+                    <div className="calendar">
+                        <div className="calendar-week">1 2 3 4</div>
+                        <div className="calendar-week">5 6 7 8 9 10 11</div>
+                        <div className="calendar-week">12 13 14 15 16 17 18</div>
+                        <div className="calendar-week">19 20 21 22 23 24 25</div>
+                        <div className="calendar-week">26 27 28 29 30 31</div>
+                    </div>
+                </div>
+                <div className="data-picker-month">
+                    <div className="date-picker-month-header">9月</div>
+                    <div className="calendar">
+                        <div className="calendar-week">1 2 3 4</div>
+                        <div className="calendar-week">5 6 7 8 9 10 11</div>
+                        <div className="calendar-week">12 13 14 15 16 17 18</div>
+                        <div className="calendar-week">19 20 21 22 23 24 25</div>
+                        <div className="calendar-week">26 27 28 29 30 31</div>
+                    </div>
+                </div>
+                <div className="data-picker-month">
+                    <div className="date-picker-month-header">10月</div>
+                    <div className="calendar">
+                        <div className="calendar-week">1 2 3 4</div>
+                        <div className="calendar-week">5 6 7 8 9 10 11</div>
+                        <div className="calendar-week">12 13 14 15 16 17 18</div>
+                        <div className="calendar-week">19 20 21 22 23 24 25</div>
+                        <div className="calendar-week">26 27 28 29 30 31</div>
+                    </div>
+                </div>
+                <div className="data-picker-month">
+                    <div className="date-picker-month-header">11月</div>
+                    <div className="calendar">
+                        <div className="calendar-week">1 2 3 4</div>
+                        <div className="calendar-week">5 6 7 8 9 10 11</div>
+                        <div className="calendar-week">12 13 14 15 16 17 18</div>
+                        <div className="calendar-week">19 20 21 22 23 24 25</div>
+                        <div className="calendar-week">26 27 28 29 30 31</div>
+                    </div>
+                </div>
+                <div className="data-picker-month">
+                    <div className="date-picker-month-header">12月</div>
+                    <div className="calendar">
+                        <div className="calendar-week">1 2 3 4</div>
+                        <div className="calendar-week">5 6 7 8 9 10 11</div>
+                        <div className="calendar-week">12 13 14 15 16 17 18</div>
+                        <div className="calendar-week">19 20 21 22 23 24 25</div>
+                        <div className="calendar-week">26 27 28 29 30 31</div>
+                    </div>
+                </div>
             </div>
         </div>
     );
