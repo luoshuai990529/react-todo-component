641b2f265269b774ab31f1974f93fbdf415c1d18
luoshuai <1029113525@qq.com>
2021-06-23 19:05:47 +0800

    feat:样式调整

--- src/DatePicker/components/datePicker.tsx
+++ src/DatePicker/components/datePicker.tsx
@@ -1,24 +1,70 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import LeftSvg from '../svg/leftSvg';
 import RightSvg from '../svg/rightSvg';
-
+import dayjs from '../utils/dayjs';
 interface PropsType {}
+const nowTime = dayjs();
 export default function DatePicker(props: PropsType) {
-    useEffect(() => {}, []);
+    const dateContentRef = useRef<HTMLDivElement>(null);
+    const [headerDate, setHeaderDate] = useState(dayjs());
+    useEffect(() => {
+        console.log(`当前${headerDate.format('M月')}月份有${headerDate.daysInMonth()}天`);
+        const dateContentEle = dateContentRef.current;
+        if (dateContentEle) {
+            dateContentEle.addEventListener('scroll', () => {
+                console.log('111');
+            });
+        }
+    }, [headerDate]);
+
+    const nextMonth = () => {
+        const nextMonth = headerDate.add(1, 'month');
+        setHeaderDate(nextMonth);
+    };
+    const lastMonth = () => {
+        const lastMonth = headerDate.subtract(1, 'month');
+        if (lastMonth.isBefore(dayjs().subtract(1, 'month'))) {
+            return;
+        }
+        setHeaderDate(lastMonth);
+    };
+    const backNowMonth = () => {
+        if (headerDate === dayjs()) return;
+        setHeaderDate(dayjs());
+    };
+    const MonthListRender = () => {
+        const arr = [1, 2, 3, 4, 5, 6, 7];
+        return (
+            <React.Fragment>
+                {arr.map((item, key) => (
+                    <div key={key} className="data-picker-month">
+                        <div className="date-picker-month-header">{5 + item}月</div>
+                        <div className="calendar">
+                            <div className="calendar-week">1 2 3 4</div>
+                            <div className="calendar-week">5 6 7 8 9 10 11</div>
+                            <div className="calendar-week">12 13 14 15 16 17 18</div>
+                            <div className="calendar-week">19 20 21 22 23 24 25</div>
+                            <div className="calendar-week">26 27 28 29 30 31</div>
+                        </div>
+                    </div>
+                ))}
+            </React.Fragment>
+        );
+    };
     return (
         <div className="scheduler-date-picker">
             <div className="scheduler-date-picker-header">
-                <div className="month">6月 2021</div>
+                <div className="month">{headerDate.format('M月 YYYY')}</div>
                 <div className="actions">
-                    <button>
+                    <button onClick={lastMonth} className={headerDate.subtract(1, 'month').isBefore(dayjs().subtract(1, 'month')) ? 'disable' : 'btn'}>
                         <LeftSvg />
                     </button>
-                    <button>
+                    <button onClick={backNowMonth} className={headerDate.subtract(1, 'month').isBefore(dayjs().subtract(1, 'month')) ? 'disable' : 'btn'}>
                         <div>
                             <div className="outline-circle"></div>
                         </div>
                     </button>
-                    <button>
+                    <button onClick={nextMonth} className="btn">
                         <RightSvg />
                     </button>
                 </div>
@@ -34,77 +80,8 @@ export default function DatePicker(props: PropsType) {
                     <div>周日</div>
                 </div>
             </div>
-            <div className="scheduler-date-picker-content">
-                <div className="data-picker-month">
-                    <div className="date-picker-month-header">6月</div>
-                    <div className="calendar">
-                        <div className="calendar-week">1 2 3 4</div>
-                        <div className="calendar-week">5 6 7 8 9 10 11</div>
-                        <div className="calendar-week">12 13 14 15 16 17 18</div>
-                        <div className="calendar-week">19 20 21 22 23 24 25</div>
-                        <div className="calendar-week">26 27 28 29 30 31</div>
-                    </div>
-                </div>
-                <div className="data-picker-month">
-                    <div className="date-picker-month-header">7月</div>
-                    <div className="calendar">
-                        <div className="calendar-week">1 2 3 4</div>
-                        <div className="calendar-week">5 6 7 8 9 10 11</div>
-                        <div className="calendar-week">12 13 14 15 16 17 18</div>
-                        <div className="calendar-week">19 20 21 22 23 24 25</div>
-                        <div className="calendar-week">26 27 28 29 30 31</div>
-                    </div>
-                </div>
-                <div className="data-picker-month">
-                    <div className="date-picker-month-header">8月</div>
-                    <div className="calendar">
-                        <div className="calendar-week">1 2 3 4</div>
-                        <div className="calendar-week">5 6 7 8 9 10 11</div>
-                        <div className="calendar-week">12 13 14 15 16 17 18</div>
-                        <div className="calendar-week">19 20 21 22 23 24 25</div>
-                        <div className="calendar-week">26 27 28 29 30 31</div>
-                    </div>
-                </div>
-                <div className="data-picker-month">
-                    <div className="date-picker-month-header">9月</div>
-                    <div className="calendar">
-                        <div className="calendar-week">1 2 3 4</div>
-                        <div className="calendar-week">5 6 7 8 9 10 11</div>
-                        <div className="calendar-week">12 13 14 15 16 17 18</div>
-                        <div className="calendar-week">19 20 21 22 23 24 25</div>
-                        <div className="calendar-week">26 27 28 29 30 31</div>
-                    </div>
-                </div>
-                <div className="data-picker-month">
-                    <div className="date-picker-month-header">10月</div>
-                    <div className="calendar">
-                        <div className="calendar-week">1 2 3 4</div>
-                        <div className="calendar-week">5 6 7 8 9 10 11</div>
-                        <div className="calendar-week">12 13 14 15 16 17 18</div>
-                        <div className="calendar-week">19 20 21 22 23 24 25</div>
-                        <div className="calendar-week">26 27 28 29 30 31</div>
-                    </div>
-                </div>
-                <div className="data-picker-month">
-                    <div className="date-picker-month-header">11月</div>
-                    <div className="calendar">
-                        <div className="calendar-week">1 2 3 4</div>
-                        <div className="calendar-week">5 6 7 8 9 10 11</div>
-                        <div className="calendar-week">12 13 14 15 16 17 18</div>
-                        <div className="calendar-week">19 20 21 22 23 24 25</div>
-                        <div className="calendar-week">26 27 28 29 30 31</div>
-                    </div>
-                </div>
-                <div className="data-picker-month">
-                    <div className="date-picker-month-header">12月</div>
-                    <div className="calendar">
-                        <div className="calendar-week">1 2 3 4</div>
-                        <div className="calendar-week">5 6 7 8 9 10 11</div>
-                        <div className="calendar-week">12 13 14 15 16 17 18</div>
-                        <div className="calendar-week">19 20 21 22 23 24 25</div>
-                        <div className="calendar-week">26 27 28 29 30 31</div>
-                    </div>
-                </div>
+            <div className="scheduler-date-picker-content" ref={dateContentRef}>
+                {MonthListRender()}
             </div>
         </div>
     );
